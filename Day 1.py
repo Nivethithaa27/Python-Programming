29abe8be2c7ccfe175f194855eb2ca6633e781db
Nivethitha Kannan <nivethithasaguthala@gmail.com>
2026-03-19 07:22:41 +0530

    Complete fresh start

---
+++ Day 1.py
@@ -0,0 +1,21 @@
+# Day 1 - Program 1: Hello World
+# This is a comment. Python ignores it.
+# Comments explain what your code does.
+
+# Variables store information
+name = "Nivethitha"
+age = 21
+city = "Sathyamangalam"
+favourite_food = "Briyani"
+favourite_sport = "cricket"
+
+# print() shows output on screen
+print("Hello World!")
+print("My name is", name)
+print("I am", age, "years old")
+print("I live in", city)
+print("I love to eat",favourite_food)
+print("I like to watch",favourite_sport)
+
+# f-string: cleaner way to print variables
+print(f"Hello! I am {name}, {age} years old girl from {city}.I love to eat {favourite_food} and Like to watch {favourite_sport}")
